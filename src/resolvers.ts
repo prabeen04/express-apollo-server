a7d811c34fa25ceacd26b1244a1f73efcc3d0d22
Prabeen04 <prabeen.strange@gmail.com>
2019-04-06 12:26:45 +0530

    Author and user resolver and schema defined

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -2,9 +2,24 @@
 import { IResolvers } from 'graphql-tools';
 const resolver: IResolvers = {
   Query: {
-    helloWorld(_: void, args: void): string {
-  return `👋 Hello world! 👋`;
+    user(_: void, args: any): any {
+      return {
+        id: `👋 Hello world! 👋`,
+        userName: `prabeen👋`,
+        email: 'example@example.com'
+      }
+    },
+    author(_: void, args: any): any {
+      return {
+        id: `👋 Hello world! 👋`,
+        name: `prabeen👋`
+      }
     },
   },
+  // Author: {
+  //   userName(_: void, args: void): string {
+  //     return `👋 Hello Prabeen! 👋`;
+  //   },
+  // },
 };
 export default resolver;

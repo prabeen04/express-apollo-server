879f7e6df00e4f8d22ba334040453499aef17351
Prabeen04 <prabeen.strange@gmail.com>
2019-04-13 18:40:41 +0530

    code cleaned

--- src/server.ts
+++ src/server.ts
@@ -13,36 +13,33 @@ function startServer() {
   mongoose.Promise = global.Promise;
   mongoose.connect("mongodb://localhost:27017/graphql", {
     useNewUrlParser: true
-  });
-  //Get the default connection
-  let conn = mongoose.createConnection("mongodb://localhost:27017/graphql");
-  //Bind connection to error event (to get notification of connection errors)
-  conn
-    .then(res => {
-      const server = new ApolloServer({
-        schema,
-        validationRules: [depthLimit(7)]
-      });
-      app.use("*", cors());
-      // body parser middleware
-      app.use(bodyParser.json());
-      app.use(
-        bodyParser.urlencoded({
-          extended: false
-        })
-      );
-      app.use(compression());
-      server.applyMiddleware({ app, path: "/graphql" });
+  }).then(res => {
+    const server = new ApolloServer({
+      schema,
+      validationRules: [depthLimit(7)]
+    });
+    app.use("*", cors());
+    // body parser middleware
+    app.use(bodyParser.json());
+    app.use(
+      bodyParser.urlencoded({
+        extended: false
+      })
+    );
+    app.use(compression());
+    server.applyMiddleware({ app, path: "/graphql" });
 
-      const httpServer = createServer(app);
-      httpServer.listen(
-        { port: 4000 },
-        (): void =>
-          console.log(
-            `\n🚀      GraphQL is now running on http://localhost:4000/graphql`
-          )
-      );
-    })
+    const httpServer = createServer(app);
+    httpServer.listen(
+      { port: 4000 },
+      (): void =>
+        console.log(
+          `\n🚀      GraphQL is now running on http://localhost:4000/graphql`
+        )
+    );
+  })
     .catch(() => console.error.bind(console, "MongoDB connection error:"));
+  ;
+
 }
 startServer();

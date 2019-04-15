5023f965debe5eb8d1aa2cde6c3da32ceaf95259
Prabeen04 <prabeen.strange@gmail.com>
2019-04-15 20:58:24 +0530

    todo schema and resolver merged

--- src/schema.ts
+++ src/schema.ts
@@ -2,13 +2,15 @@ import "graphql-import-node";
 import { mergeResolvers, mergeTypes } from "merge-graphql-schemas";
 import * as typeDefs from "./schema/schema.graphql";
 import * as registerTypedefs from "./modules/auth/register/schema.graphql";
-import * as registerResolver from './modules/auth/register/registerResolver'
-import { makeExecutableSchema, mergeSchemas } from "graphql-tools";
+import * as registerResolver from "./modules/auth/register/registerResolver";
+import * as todoTypedefs from "./modules/auth/todo/schema.graphql";
+import * as todoResolver from "./modules/todo/todoResolver";
+import { makeExecutableSchema } from "graphql-tools";
 import resolvers from "./resolvers";
 import { GraphQLSchema } from "graphql";
 
 const schema: GraphQLSchema = makeExecutableSchema({
-  typeDefs: mergeTypes([typeDefs, registerTypedefs]),
-  resolvers: mergeResolvers([resolvers, registerResolver])
+  typeDefs: mergeTypes([typeDefs, registerTypedefs, todoTypedefs]),
+  resolvers: mergeResolvers([resolvers, registerResolver, todoResolver])
 });
 export default schema;

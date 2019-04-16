07a5379f31c03144f745c656cb2ebef8d6d7edbe
Prabeen04 <prabeen.strange@gmail.com>
2019-04-16 19:15:30 +0530

    register schema and resolver

--- src/modules/auth/register/registerResolver.ts
+++ src/modules/auth/register/registerResolver.ts
@@ -20,3 +20,5 @@ export const userResolver: IResolvers = {
     }
   }
 };
+
+// export default userResolver;
--- src/schema.ts
+++ src/schema.ts
@@ -1,16 +1,18 @@
 import "graphql-import-node";
+import { GraphQLSchema } from "graphql";
 import { mergeResolvers, mergeTypes } from "merge-graphql-schemas";
-import * as typeDefs from "./schema/schema.graphql";
+// import * as typeDefs from "./schema/schema.graphql";
 import * as registerTypedefs from "./modules/auth/register/schema.graphql";
 import * as registerResolver from "./modules/auth/register/registerResolver";
+
 import * as todoTypedefs from "./modules/todo/schema.graphql";
 import * as todoResolver from "./modules/todo/todoResolver";
+
 import { makeExecutableSchema } from "graphql-tools";
-import resolvers from "./resolvers";
-import { GraphQLSchema } from "graphql";
+// import resolvers from "./resolvers";
 
 const schema: GraphQLSchema = makeExecutableSchema({
-  typeDefs: mergeTypes([typeDefs, registerTypedefs, todoTypedefs]),
-  resolvers: mergeResolvers([resolvers, registerResolver, todoResolver])
+  typeDefs: mergeTypes([registerTypedefs, todoTypedefs]),
+  resolvers: mergeResolvers([registerResolver, todoResolver])
 });
 export default schema;

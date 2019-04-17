5ff3abced3529494e92eb699d7aff1b5536f9aa9
Prabeen04 <prabeen.strange@gmail.com>
2019-04-17 22:49:22 +0530

    reverted

--- src/schema.ts
+++ src/schema.ts
@@ -1,25 +1,20 @@
 import "graphql-import-node";
 import { GraphQLSchema } from "graphql";
-import * as RequireGraphqlFile from 'require-graphql-file';
 import { mergeResolvers, mergeTypes } from "merge-graphql-schemas";
-// import * as userSchema from './modules/auth/shared/schema.graphql'
-// import * as registerTypedefs from "./modules/auth/register/schema.graphql";
+import * as userSchema from './modules/auth/shared/schema.graphql'
+import * as registerTypedefs from "./modules/auth/register/schema.graphql";
 import registerResolver from "./modules/auth/register/registerResolver";
 
-// import * as todoTypedefs from "./modules/todo/schema.graphql";
+import * as todoTypedefs from "./modules/todo/schema.graphql";
 import todoResolver from "./modules/todo/todoResolver";
 
-// import * as storyTypedefs from "./modules/story/schema.graphql";
+import * as storyTypedefs from "./modules/story/schema.graphql";
 import storyResolver from "./modules/story/storyResolver";
 
 import { makeExecutableSchema } from "graphql-tools";
-const userSchema = RequireGraphqlFile('./modules/auth/shared/schema.graphql')
-const registerTypedefs = RequireGraphqlFile('../modules/auth/register/schema.graphql')
-const todoTypedefs = RequireGraphqlFile('./modules/todo/schema.graphql')
-const storyTypedefs = RequireGraphqlFile('./modules/story/schema.graphql')
 
 const schema: GraphQLSchema = makeExecutableSchema({
-  typeDefs: mergeTypes([userSchema, registerTypedefs, todoTypedefs,storyTypedefs], { all: true }),
+  typeDefs: mergeTypes([userSchema, registerTypedefs, todoTypedefs,storyTypedefs ], { all: true }),
   resolvers: mergeResolvers([registerResolver, todoResolver, storyResolver])
 });
 export default schema;

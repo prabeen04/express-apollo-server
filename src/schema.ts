2dbbe2367a3aa26ae13f5714e2d435d3aeb05780
Prabeen04 <prabeen.strange@gmail.com>
2019-04-18 21:35:20 +0530

    fileLoader added

--- src/schema.ts
+++ src/schema.ts
@@ -1,6 +1,7 @@
 import "graphql-import-node";
+import * as path from "path";
 import { GraphQLSchema } from "graphql";
-import { mergeResolvers, mergeTypes } from "merge-graphql-schemas";
+import { mergeResolvers, mergeTypes, fileLoader } from "merge-graphql-schemas";
 import * as userSchema from './modules/auth/shared/schema.graphql'
 import * as registerTypedefs from "./modules/auth/register/schema.graphql";
 import registerResolver from "./modules/auth/register/registerResolver";
@@ -12,9 +13,9 @@ import * as storyTypedefs from "./modules/story/schema.graphql";
 import storyResolver from "./modules/story/storyResolver";
 
 import { makeExecutableSchema } from "graphql-tools";
-
+const typesArray = fileLoader(path.join(__dirname, '.'), { recursive: true })
 const schema: GraphQLSchema = makeExecutableSchema({
-  typeDefs: mergeTypes([userSchema, registerTypedefs, todoTypedefs,storyTypedefs ], { all: true }),
+  typeDefs: mergeTypes(typesArray, { all: true }),
   resolvers: mergeResolvers([registerResolver, todoResolver, storyResolver])
 });
 export default schema;

a8c92e0becea54917318f99194a8e0008f1f6abb
Prabeen04 <prabeen.strange@gmail.com>
2019-04-19 22:03:49 +0530

    code cleaned

--- src/schema.ts
+++ src/schema.ts
@@ -2,18 +2,12 @@ import "graphql-import-node";
 import * as path from "path";
 import { GraphQLSchema } from "graphql";
 import { mergeResolvers, mergeTypes, fileLoader } from "merge-graphql-schemas";
-import * as userSchema from './modules/auth/shared/schema.graphql'
-import * as registerTypedefs from "./modules/auth/register/schema.graphql";
 import registerResolver from "./modules/auth/register/registerResolver";
-
-import * as todoTypedefs from "./modules/todo/schema.graphql";
 import todoResolver from "./modules/todo/todoResolver";
-
-import * as storyTypedefs from "./modules/story/schema.graphql";
 import storyResolver from "./modules/story/storyResolver";
 
 import { makeExecutableSchema } from "graphql-tools";
-const typesArray = fileLoader(path.join(__dirname, '.'), { recursive: true })
+const typesArray = fileLoader(path.join(__dirname, 'modules/**/*.graphql'), { recursive: true })
 const schema: GraphQLSchema = makeExecutableSchema({
   typeDefs: mergeTypes(typesArray, { all: true }),
   resolvers: mergeResolvers([registerResolver, todoResolver, storyResolver])

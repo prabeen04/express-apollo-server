0abeea597c2869a5c465e6ed2dc5e9c89ec3781b
Prabeen04 <prabeen.strange@gmail.com>
2019-04-20 14:26:00 +0530

    manual schema stiching

--- src/schema.ts
+++ src/schema.ts
@@ -5,9 +5,15 @@ import { mergeResolvers, mergeTypes, fileLoader } from "merge-graphql-schemas";
 import registerResolver from "./modules/auth/register/registerResolver";
 import todoResolver from "./modules/todo/todoResolver";
 import storyResolver from "./modules/story/storyResolver";
-
+import * as sharedSchema from './modules/auth/shared/schema.graphql'
+import * as registerSChema from './modules/auth/register/schema.graphql'
+import * as loginSchema from './modules/auth/login/schema.graphql'
+import * as todoSchema from './modules/todo/schema.graphql'
+import * as storySchema from './modules/story/schema.graphql'
 import { makeExecutableSchema } from "graphql-tools";
-const typesArray = fileLoader(path.join(__dirname, 'modules/**/*.graphql'), { recursive: true })
+// const typesArray = fileLoader(path.join(__dirname, './modules/**/*.graphql'), { recursive: true })
+const typesArray: any[] = [sharedSchema, registerSChema, loginSchema, todoSchema, storySchema]
+
 const schema: GraphQLSchema = makeExecutableSchema({
   typeDefs: mergeTypes(typesArray, { all: true }),
   resolvers: mergeResolvers([registerResolver, todoResolver, storyResolver])

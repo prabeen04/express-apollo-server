5b7148af23029f801759126eddb4f85f26625b0e
Prabeen04 <prabeen.strange@gmail.com>
2019-04-23 21:30:13 +0530

    token generated on login

--- src/modules/auth/login/loginResolver.ts
+++ src/modules/auth/login/loginResolver.ts
@@ -1,18 +1,21 @@
 import { IResolvers } from "graphql-tools";
 import { omit } from "lodash";
-import  jwt from 'jsonwebtoken';
 import * as bcryptjs from "bcryptjs";
+import * as jwt from 'jsonwebtoken';
 import User from "../../../models/UserSchema";
 
 const loginResolver: IResolvers = {
   Mutation: {
     login: async (_: any, args: any) => {
-        const { email, password } = args;
-        const user: any = await User.findOne({ email })
-        if(!user) return false
-        const match = await bcryptjs.compare(password, user.password)
-        jwt.
-        return match ? true : false
+      const { email, password } = args;
+      const user: any = await User.findOne({ email })
+      if (!user) return false
+      const match = await bcryptjs.compare(password, user.password)
+      console.log(match)
+      const token = jwt.sign(user.toObject(), 'process.env.CLIENT_SECRET')
+      console.log(token)
+      if (!match) return false
+      return true
     }
   }
 };

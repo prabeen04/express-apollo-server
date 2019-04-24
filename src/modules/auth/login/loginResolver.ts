accb27ff080c7efc34eeeda8a5cf6789eef580da
Prabeen04 <prabeen.strange@gmail.com>
2019-04-24 09:22:12 +0530

    process.env configuration

--- src/modules/auth/login/loginResolver.ts
+++ src/modules/auth/login/loginResolver.ts
@@ -1,8 +1,15 @@
 import { IResolvers } from "graphql-tools";
 import { omit } from "lodash";
 import * as bcryptjs from "bcryptjs";
+
 import * as jwt from 'jsonwebtoken';
 import User from "../../../models/UserSchema";
+
+declare var process: {
+  env: {
+    CLIENT_SECRET: string
+  }
+}
 
 const loginResolver: IResolvers = {
   Mutation: {
@@ -12,10 +19,13 @@ const loginResolver: IResolvers = {
       if (!user) return false
       const match = await bcryptjs.compare(password, user.password)
       console.log(match)
-      const token = jwt.sign(user.toObject(), 'process.env.CLIENT_SECRET')
+      const token = jwt.sign(user.toObject(), process.env['CLIENT_SECRET'])
       console.log(token)
-      if (!match) return false
-      return true
+      if (!match) return { status: false }
+      return {
+        status: true,
+        token
+      }
     }
   }
 };

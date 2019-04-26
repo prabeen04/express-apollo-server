7fc99ba933d3009113bf7557e6e553b07c825e88
Prabeen04 <prabeen.strange@gmail.com>
2019-04-26 09:16:03 +0530

    accountresolver created

---
+++ src/modules/account/accountResolver.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import { IResolvers } from "graphql-tools";
+import https from 'https';
+import { IAccount, IContact } from "../contact/contactInterface";
+// At request level
+const agent = new https.Agent({
+    rejectUnauthorized: false
+});
+
+const accountResolver: IResolvers = {
+    Query: {
+        accounts: async (_: any, args: any, { req }) => {
+            const { token } = req.session;
+            try {
+                const { data }: any = await axios.get(`${process.env.API_URI}/accounts`, {
+                    httpsAgent: agent,
+                    headers: {
+                        'Authorization': 'Bearer ' + token
+                    }
+                })
+                console.log(data)
+                return data
+            } catch (e) {
+                console.log(e)
+                return 
+            }
+        },
+    }
+};
+
+export default accountResolver;

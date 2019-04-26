2930d0c39de62aff2f78c42b1b582c20cba84fca
Prabeen04 <prabeen.strange@gmail.com>
2019-04-26 09:32:33 +0530

    bug fixes

--- src/schema.ts
+++ src/schema.ts
@@ -7,12 +7,13 @@ import loginRegister from './modules/auth/login/loginResolver'
 import todoResolver from "./modules/todo/todoResolver";
 import storyResolver from "./modules/story/storyResolver";
 import contactResolver from './modules/contact/contactResolver';
+import accountResolver from './modules/account/accountResolver';
 const typesArray = fileLoader(path.join(__dirname, './modules/**/*.graphql'), { recursive: true })
 
 import { makeExecutableSchema } from "graphql-tools";
 
 const schema: GraphQLSchema = makeExecutableSchema({
   typeDefs: mergeTypes(typesArray, { all: true }),
-  resolvers: mergeResolvers([registerResolver, loginRegister, todoResolver, storyResolver, contactResolver])
+  resolvers: mergeResolvers([registerResolver, loginRegister, todoResolver, storyResolver, contactResolver, accountResolver])
 });
 export default schema;

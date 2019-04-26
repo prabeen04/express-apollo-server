2b28d30b231adbbc0a61cea220fadabc1f55f049
Prabeen04 <prabeen.strange@gmail.com>
2019-04-26 09:17:41 +0530

    account interface createdd

---
+++ src/modules/account/accountInterface.ts
@@ -0,0 +1,26 @@
+
+export interface IAccount {
+    accountId: String
+    accountName: String
+    accountOwner: String
+    website: String
+    tickerSymbol: String
+    tradeCurrency: String
+    companySize: String
+    industryType: String
+    baseCountry: String
+    phone1: String
+    phone2: String
+    fax: String
+    email: String
+    rating: String
+    sicCode: String
+    annualRevenue: String
+    description: String
+    creator: String
+    creationDate: String
+    userId: String
+    organizationId: String
+    imageId: String
+    imageURL: String
+}

fcf9e94eacd0da118f4976a1657e4918098ac488
Prabeen04 <prabeen.strange@gmail.com>
2019-05-18 12:48:37 +0530

    date type changed to string

--- src/models/StorySchema.ts
+++ src/models/StorySchema.ts
@@ -5,7 +5,7 @@ const storySchema: Schema = new Schema({
     title: { type: String, required: true },
     article: { type: String, required: true },
     authorId: { type: String, required: true },
-    createdAt: { type: Date, required: true }
+    createdAt: { type: String, required: true }
 })
 
 export default mongoose.model<IStory>('story', storySchema)
